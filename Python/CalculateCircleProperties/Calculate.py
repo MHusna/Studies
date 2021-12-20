f3d4d656c3bb040002bc73cd2943823e01a5939c
MHusna <mehmethusna@outlook.com>
2021-12-20 13:23:26 +0300

    Python file created.

---
+++ Python/CalculateCircleProperties/Calculate.py
@@ -0,0 +1,7 @@
+radius = float(input("Enter the Radius of Circle: "))
+pi = 3.14
+
+field = pi * (radius ** 2)
+circumference = 2 * pi * radius
+
+print("Field: "+str(field)+" Circumference: "+str(circumference))
