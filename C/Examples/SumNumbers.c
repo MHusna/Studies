d178e8efdedbdcb762a5e3a7ff997e8a0473735f
MHusna <mehmethusna@outlook.com>
2021-12-29 17:12:29 +0300

    SumNumbers commited.

---
+++ C/Examples/SumNumbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	
+	int var1, var2, result;
+	
+	printf("First Number: ");
+	scanf("%d",&var1);
+	
+	printf("Second Number: ");
+	scanf("%d",&var2);	
+
+	result = var1 + var2;
+	printf("Result: %d",result);
+	return 0;
+
+}
