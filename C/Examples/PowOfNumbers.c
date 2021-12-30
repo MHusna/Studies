0308905c8e2a10db2d1cd5823c0d748fd742db23
MHusna <mehmethusna@outlook.com>
2021-12-30 12:28:56 +0300

    Pow of numbers commited.

---
+++ C/Examples/PowOfNumbers.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+	int number = 0;
+	printf("Number\tPow(2)\tPow(3)");
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+	
+	printf("\n%d\t%d\t%d",(number),(number*number),(number*number*number));
+	number = number + 1;
+}
