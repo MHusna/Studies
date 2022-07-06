afb1896ec8d1b918cc536d5513c0a8af483d076c
MHusna <48207418+MHusna@users.noreply.github.com>
2022-07-06 17:22:39 +0300

    C#

---
+++ WinForms/05_07_2022_PerformansCounter/05_07_2022_PerformansCounter/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_07_2022_PerformansCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = (int) performanceCounter1.NextValue();
+            progressBar2.Value = (int) performanceCounter2.NextValue();
+        }
+    }
+}
